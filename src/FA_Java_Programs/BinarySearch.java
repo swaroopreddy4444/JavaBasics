ff1513bd7cba72fa30d3efa4f7c4ff2821718f42
swaroopreddy4444 <swaroopreddy4444@gmail.com>
2022-05-20 08:21:20 +0530

    Update BinarySearch.java

--- src/FA_Java_Programs/BinarySearch.java
+++ src/FA_Java_Programs/BinarySearch.java
@@ -8,25 +8,26 @@ public class BinarySearch {
 		
 		int[] a= {2,3,4,5,6,7,8,9};
 		
-		int x=9;
+		int x=7;
+// METHOD 1		
+		int low=0;
+		int high=a.length-1;
+		boolean flag=false;
+		while(low<=high) {
+			int mid=(low+high)/2;
+			if(a[mid]==x) {
+				System.out.println("found "+x+" at index "+mid);
+				flag=true;
+				break;
+			}
+			low=(a[mid]<x)?mid+1:low;
+			high=(a[mid]>x)?mid-1:high;		
+		}
+		if(flag==false) {
+			System.out.println(x+ " Element not found");
+		}
 		
-//		int low=0;
-//		int high=a.length-1;
-//		boolean flag=false;
-//		while(low<=high) {
-//			int mid=(low+high)/2;
-//			if(a[mid]==x) {
-//				System.out.println("found "+x+" at index "+mid);
-//				flag=true;
-//				break;
-//			}
-//			low=(a[mid]<x)?mid+1:low;
-//			high=(a[mid]>x)?mid-1:high;		
-//		}
-//		if(flag==false) {
-//			System.out.println(x+ " Element not found");
-//		}
-		
+		// METHOD 2		
 		int index=Arrays.binarySearch(a, x);
 		if(index>=0)
 			System.out.println("found "+x+" at index "+index);
